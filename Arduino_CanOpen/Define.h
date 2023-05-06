130235e1e039a5312c172f7a9e0ebeb761136401
Lautaro Mendieta <lemendieta96@gmail.com>
2023-05-05 23:45:03 -0300

    Add source files

---
+++ Arduino_CanOpen/Define.h
@@ -0,0 +1,57 @@
+
+#define Can_nodeid_client1 0x02
+#define Can_nodeid_client2 0x03
+#define Can_nodeid_client3 0x04
+#define Can_nodeid_client4 0x05
+#define Can_nodeid_client5 0x06
+#define Can_nodeid_client6 0x07
+
+#define M1_Z_PIN 2
+#define M1_nBits 12
+#define M1_CPT 4096
+#define M1_ratioGB 231
+#define M1_En 9
+#define M1_in1 22
+#define M1_in2 38
+
+#define M2_Z_PIN 3
+#define M2_nBits 12
+#define M2_CPT 4096
+#define M2_ratioGB 231
+#define M2_En 8
+#define M2_in1 24
+#define M2_in2 40
+
+#define M3_Z_PIN 18
+#define M3_nBits 12
+#define M3_CPT 4096
+#define M3_ratioGB 231
+#define M3_En 7
+#define M3_in1 26
+#define M3_in2 42
+
+#define M4_Z_PIN 19
+#define M4_nBits 12
+#define M4_CPT 4096
+#define M4_ratioGB 231
+#define M4_En 6
+#define M4_in1 28
+#define M4_in2 44
+
+#define M5_Z_PIN 20
+#define M5_nBits 12
+#define M5_CPT 4096
+#define M5_ratioGB 231
+#define M5_En 5
+#define M5_in1 30
+#define M5_in2 46
+
+#define M6_Z_PIN 21
+#define M6_nBits 12
+#define M6_CPT 4096
+#define M6_ratioGB 231
+#define M6_En 4
+#define M6_in1 32
+#define M6_in2 48
+
+#define ModuloCAN_INTERRUPT_PIN 14
