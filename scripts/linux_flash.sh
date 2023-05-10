a9209a136a6a35e8e8bca0e02314a6a0ebe8b981
Lautaro Mendieta <lemendieta96@gmail.com>
2023-05-09 23:38:58 -0300

    Add configurable port for flash script

--- scripts/linux_flash.sh
+++ scripts/linux_flash.sh
@@ -2,8 +2,9 @@
 
 readonly PROJECT_DIRECTORY="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && cd .. && pwd)"
 readonly IMAGE_NAME='ghcr.io/spacebee-technologies/arduino-environment:v0.1.0'
+readonly PORT="${1:-/dev/ttyACM0}"
 
 docker run --privileged --rm -t -i -v "${PROJECT_DIRECTORY}:/workspace" \
            -w /workspace -v /dev:/dev "${IMAGE_NAME}" \
-           bash -c 'arduino-cli core install arduino:avr;
-                    arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:mega -t -v --input-dir build'
+           bash -c "arduino-cli core install arduino:avr;
+                    arduino-cli upload -p ${PORT} --fqbn arduino:avr:mega -t -v --input-dir build"
