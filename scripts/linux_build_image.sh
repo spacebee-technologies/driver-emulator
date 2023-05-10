2426d952a8c71fc4c466fa6b6ff484aec4b5cb7e
Lautaro Mendieta <lemendieta96@gmail.com>
2023-05-09 23:23:29 -0300

    Remove local image for build and flash

--- scripts/linux_build_image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-readonly PROJECT_DIRECTORY="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && cd .. && pwd)"
-
-cd "${PROJECT_DIRECTORY}/docker"
-docker build -t arduino-cli-docker:local .
