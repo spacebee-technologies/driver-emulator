5c2e1b6fd192728d5215496b8136d35a31c09322
Lautaro Mendieta <54816650+lautaromendieta@users.noreply.github.com>
2023-05-30 22:50:37 -0300

    Update build and flash scripts (#10)

--- scripts/linux_flash.sh
+++ scripts/linux_flash.sh
@@ -5,6 +5,6 @@ readonly IMAGE_NAME='ghcr.io/spacebee-technologies/arduino-environment:v0.1.0'
 readonly PORT="${1:-/dev/ttyACM0}"
 
 docker run --privileged --rm -t -i -v "${PROJECT_DIRECTORY}:/workspace" \
-           -w /workspace -v /dev:/dev "${IMAGE_NAME}" \
+           -w /workspace -v /dev:/dev --network host "${IMAGE_NAME}" \
            bash -c "arduino-cli core install arduino:avr;
                     arduino-cli upload -p ${PORT} --fqbn arduino:avr:mega -t -v --input-dir build"
