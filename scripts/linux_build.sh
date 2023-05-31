4c9523bb49e55488d337c8e9ef91b7c55cc01433
Lautaro Mendieta <lemendieta96@gmail.com>
2023-05-30 22:49:38 -0300

    Update build and flash scripts

--- scripts/linux_build.sh
+++ scripts/linux_build.sh
@@ -4,7 +4,7 @@ readonly PROJECT_DIRECTORY="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/
 readonly IMAGE_NAME='ghcr.io/spacebee-technologies/arduino-environment:v0.1.0'
 
 docker run --privileged --rm -t -i -v "${PROJECT_DIRECTORY}:/workspace" \
-           -w /workspace "${IMAGE_NAME}" \
+           -w /workspace --network host "${IMAGE_NAME}" \
            bash -c 'arduino-cli lib install autowp-mcp2515;
                     arduino-cli core install arduino:avr;
                     arduino-cli compile --fqbn arduino:avr:mega --output-dir build Arduino_CanOpen/'
