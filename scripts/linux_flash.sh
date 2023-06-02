2120233b689118b511c98f2cca3f6d0742d665e8
Lautaro Mendieta <54816650+lautaromendieta@users.noreply.github.com>
2023-06-02 00:56:44 -0300

    Update scripts (#14)

--- scripts/linux_flash.sh
+++ scripts/linux_flash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 readonly PROJECT_DIRECTORY="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && cd .. && pwd)"
-readonly IMAGE_NAME='ghcr.io/spacebee-technologies/arduino-environment:develop'
+readonly IMAGE_NAME='ghcr.io/spacebee-technologies/arduino-environment:v0.2.0'
 readonly PORT="${1:-/dev/ttyACM0}"
 
 docker run --privileged --rm -t -i -v "${PROJECT_DIRECTORY}:/workspace" \
